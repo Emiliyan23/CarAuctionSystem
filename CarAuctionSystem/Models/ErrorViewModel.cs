5fa5caa65fef9f1d502ef88a85a15250f5241632
Emiliyan23 <emiliqn.slav@gmail.com>
2023-06-23 20:09:37 +0300

    Add project files.

---
+++ CarAuctionSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarAuctionSystem.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
