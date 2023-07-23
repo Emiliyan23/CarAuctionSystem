355aff7b323f029ccd433dfb0bb720e3a4ba0915
Emiliyan23 <emiliqn.slav@gmail.com>
2023-07-23 11:52:22 +0300

    Added hide statistics button.

--- CarAuctionSystem/wwwroot/js/site.js
+++ CarAuctionSystem/wwwroot/js/site.js
@@ -3,13 +3,24 @@
         e.preventDefault();
         e.stopPropagation();
 
-        $.get('https://localhost:7156/api/statistics', function (data) {
-            $('#auctions_count').text(data.auctionsCount + " Auctions");
-            $('#sales_count').text(data.salesCount + " Cars sold");
-            $('#bids_count').text(data.bidsCount + " Total bids");
+        if ($('#statistics_box').hasClass('d-none')) {
+            $.get('https://localhost:7156/api/statistics', function (data) {
+                $('#auctions_count').text(data.auctionsCount + " Auctions");
+                $('#sales_count').text(data.salesCount + " Cars sold");
+                $('#bids_count').text(data.bidsCount + " Total bids");
 
-            $('#statistics_box').removeClass('d-none');
-            $('#statistics_btn').hide();
-        });
+                $('#statistics_box').removeClass('d-none');
+
+                $('#statistics_btn').text('Hide Statistics');
+                $('#statistics_btn').removeClass('btn-primary');
+                $('#statistics_btn').addClass('btn-danger');
+            });
+        } else {
+            $('#statistics_box').addClass('d-none');
+
+            $('#statistics_btn').text('Show Statistics');
+            $('#statistics_btn').removeClass('btn-danger');
+            $('#statistics_btn').addClass('btn-primary');
+        }
 	});
 }
